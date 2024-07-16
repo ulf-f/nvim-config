8ca41628e1429a8be9ad55360d5a4d1455a22fad
Ulf <codeberg@liesdiemanpage.de>
2024-07-16 16:12:09 +0200

    init.lua: switched to new spec/syntax of key which-key

--- init.lua
+++ init.lua
@@ -304,19 +304,21 @@ require("lazy").setup({
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader>c", desc = "[C]ode" },
+				{ "<leader>d", desc = "[D]ocument" },
+				{ "<leader>r", desc = "[R]ename" },
+				{ "<leader>s", desc = "[S]earch" },
+				{ "<leader>w", desc = "[W]orkspace" },
+				{ "<leader>t", desc = "[T]oggle" },
+				{ "<leader>h", desc = "Git [H]unk" },
+				{
+					mode = { "n", "v" },
+					{ "<leader>h", desc = "Git [H]unk" },
+					{ "<leader>q", "<cmd>q<cr>", desc = "Quit" },
+					{ "<leader>w", "<cmd>w<cr>", desc = "Write" },
+				},
 			})
-			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
-			}, { mode = "v" })
 		end,
 	},
 
