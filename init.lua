35929a1da2ff3917346123f0bbd1716cd4998d41
Ulf <codeberg@liesdiemanpage.de>
2024-11-11 21:11:51 +0100

    init.lua: added back line numbers and relative numbers

--- init.lua
+++ init.lua
@@ -103,10 +103,10 @@ vim.g.loaded_netrwPlugin = 1
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
-vim.opt.number = false
+vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = ""
